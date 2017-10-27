37aac60145d4ff919d4a3dc0430b206dd16ff167
iwagaki <iwagaki@users.noreply.github.com>
2017-10-27 09:06:26 +0900

    Update with <iframe>

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@ JS_SRC = repeatplay.js
 COMPILED = compiled.js
 TARGET = bookmarklet.js
 
-CLOSURE_COMPILER_DIR = /opt/google/closure-compiler
+CLOSURE_COMPILER = /opt/google/compiler/closure-compiler-v20170910.jar
+
 
 .PHONY: all
 all: $(JS_SRC)
-	java -jar $(CLOSURE_COMPILER_DIR)/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js $(JS_SRC) --js_output_file $(COMPILED)
+	java -jar $(CLOSURE_COMPILER) --compilation_level ADVANCED_OPTIMIZATIONS --js $(JS_SRC) --js_output_file $(COMPILED)
 	cat header.js $(COMPILED) footer.js | tr -d '\n' > $(TARGET)
 	rm -f $(COMPILED)
 
--- repeatplay.js
+++ repeatplay.js
@@ -1,8 +1,6 @@
+var m;
 if (m = location.href.match(/https?:\/\/www\.youtube\.com\/watch\?v=([^&]+)/)) {
   var id = m[1];
-  var url = "http://www.youtube.com/v/" + id + "?version=3&loop=1&autoplay=1&playlist=" + id;
-  console.dir("location.href=" + location.href + ", url=" + url);
-
-  var html = '<html><head><style type="text/css">body { margin: 0; padding: 0; }</style></head><body><iframe id="ytplayer" type="text/html" width="100%" height="100%" src="' + url + '" frameborder="0" scrolling="0" /></body>'
-  top.location.href = 'data:text/html;charset=utf-8,'+encodeURIComponent(html);
+  var url = 'https://www.youtube.com/embed/' + id + '?autoplay=1&loop=1&rel=0&showinfo=0&playlist=' + id;
+  document.getElementById('player-container').innerHTML = '<iframe id="ytplayer" type="text/html" width="100%" height="100%" src="' + url + '" frameborder="0" allowfullscreen>';
 }
