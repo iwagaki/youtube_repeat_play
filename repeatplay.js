2c3d68bbbb83a0ad0d53e18ee6c168c320f34ea5
iwagaki <iwagaki@users.noreply.github.com>
2017-10-28 10:23:38 +0900

    Small fix

--- repeatplay.js
+++ repeatplay.js
@@ -1,6 +1,7 @@
-var m;
-if (m = location.href.match(/https?:\/\/www\.youtube\.com\/watch\?v=([^&]+)/)) {
+var m = location.href.match(/https?:\/\/www\.youtube\.com\/watch\?v=([^&]+)/);
+if (m) {
   var id = m[1];
   var url = 'https://www.youtube.com/embed/' + id + '?autoplay=1&loop=1&rel=0&showinfo=0&playlist=' + id;
-  document.getElementById('player-container').innerHTML = '<iframe id="ytplayer" type="text/html" width="100%" height="100%" src="' + url + '" frameborder="0" allowfullscreen>';
+  document.getElementById('player-container').innerHTML =
+    '<iframe id="ytplayer" type="text/html" width="100%" height="100%" src="' + url + '" frameborder="0" allowfullscreen>';
 }
